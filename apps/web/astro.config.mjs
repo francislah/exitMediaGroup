59e0ef463294b4e7a6de8dcf54e1798d4b392314
Francis Lahoud <francis.lahoud@proton.me>
2026-03-01 22:21:28 -0500

    update build type

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({ mode: "standalone" }),
+  output: "static",
+  // adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
